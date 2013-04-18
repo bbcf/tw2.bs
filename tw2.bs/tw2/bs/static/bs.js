62b25ed0f0e3b21d308dd5459cb81645f7b794d0
Yohan Jarosz <yohanjarosz@yahoo.fr>
2013-04-18 15:19:49 +0200

    remove comments from javascript file

--- tw2.bs/tw2/bs/static/bs.js
+++ tw2.bs/tw2/bs/static/bs.js
@@ -2,19 +2,14 @@ bs_init_file_field = function(compound_id, select){
     var selector = '#' + compound_id.split(':').join('\\:');
     $(selector).attr('name', '');
     var $root = $(selector + '\\:bs_container');
-    // hide not desired inputs
     $root.find('input:not(:radio):not(:' + select + ')').attr('name', '').hide();
-    // show desired one
     $root.find('input:' + select + '').attr('name', compound_id).show();
-    // check the right radio button
     $root.find('input:radio.bsradio_' + select).attr('checked', 'checked');
-    //connect the radio buttons
     $root.find(':radio').change(function(){
         var val = $(this).val();
         $root.find('input:not(:radio):not(:'+ val + ')').attr('name', '').hide();
         $root.find('input:' + val + '').attr('name', compound_id).show();
     });
-    // connect the values changes
      $root.find('input:not(:radio)').change(function(){
         $(selector).trigger("change");
         var row_id = compound_id.substring(0, compound_id.lastIndexOf(':'));
@@ -32,18 +27,14 @@ bs_init_triple_file_field = function(compound_id, select){
     var selector = '#' + compound_id.split(':').join('\\:');
     var $root = $(selector + '\\:bs_container');
     if (select != 'select'){
-        // hide select && show input desired
         $root.find('select').attr('name', '').hide();
         $root.find('input:' + select + '').attr('name', compound_id).show();
-        // hide not desired inputs
         $root.find('input:not(:radio):not(:' + select + ')').attr('name', '').hide();
     } else {
         $root.find('input:not(:radio)').attr('name', '').hide();
     }
-    // check the right radio button
     $root.find('input:radio.bsradio_' + select).attr('checked', 'checked');
 
-    //connect the radio buttons
     $root.find(':radio').change(function(){
         var val = $(this).val();
         if (val == 'select'){
@@ -55,7 +46,6 @@ bs_init_triple_file_field = function(compound_id, select){
             $root.find('input:not(:radio):not(:' + val + ')').attr('name', '').hide();
         }
     });
-    // connect the values changes
      $root.find('input:not(:radio), select').change(function(){
         $(selector).trigger("change");
         var row_id = compound_id.substring(0, compound_id.lastIndexOf(':'));
